c6d3d188e75aae84884bf0ca8057c6c4634bb0d7
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-18 20:41:50 +0100

    Revert "Add modulo 2 lesson 1 trials folder"
    This reverts commit 87f91e4c6b21d3d890058a3fd45ee8929ae6105b.

--- modulo02-JS/1-intro-programacion/0-pruebas/js/main.js
+++
@@ -1,4 +0,0 @@
-'use strict'; //sirve para mejorar la rapidez de ejecución del código y hará que el navegador nos muestre errores que, de no ponerla, no lo haría
-
-//Cada statement en una línea SIEMPRE
-document.querySelector('h1').innerHTML = '¡Hola mundo!';
