effe8811c722c267c433fed635e984162fe46c1b
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-19 00:45:45 +0100

    Add exercise 06

---
+++ modulo02-JS/1-intro-programacion/ejercicio06/main.js
@@ -0,0 +1,4 @@
+'use strict';
+const lipsumElement = document.querySelector('.lipsum');
+const content = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150" alt="placeholder" ><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>';
+lipsumElement.innerHTML = content;
