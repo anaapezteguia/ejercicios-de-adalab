ae6a1d042df35fe754278faec7bb5b0987f7fc5a
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-18 23:43:10 +0100

    Update exercise

--- modulo02-JS/1-intro-programacion/ejercicio03/main.js
+++ modulo02-JS/1-intro-programacion/ejercicio03/main.js
@@ -1,11 +1,3 @@
 'use strict';
 const textElement = document.querySelector ('.myParagraph');
 textElement.innerHTML = textElement.innerHTML + ' Mundo.';
-
-const firstAdalaber = document.querySelector ('.firstAdalaber');
-const secondAdalaber = document.querySelector ('.secondAdalaber');
-const thirdAdalaber = document.querySelector ('.thirdAdalaber');
-const fourthAdalaber = document.querySelector ('.fourthAdalaber');
-let selectedAdalaber = document.querySelector ('.selectedAdalaber');
-//selectedAdalaber.innerHTML = selectedAdalaber.innerHTML + firstAdalaber.innerHTML;
-selectedAdalaber.innerHTML = selectedAdalaber.innerHTML + secondAdalaber.innerHTML;
