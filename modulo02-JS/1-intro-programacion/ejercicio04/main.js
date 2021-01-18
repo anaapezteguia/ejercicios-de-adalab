fe3bc27e2dfe182a45cfad38d72512b6165f6296
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-18 23:44:00 +0100

    Add exercise 04

---
+++ modulo02-JS/1-intro-programacion/ejercicio04/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const firstAdalaber = document.querySelector ('.firstAdalaber');
+const secondAdalaber = document.querySelector ('.secondAdalaber');
+const thirdAdalaber = document.querySelector ('.thirdAdalaber');
+const fourthAdalaber = document.querySelector ('.fourthAdalaber');
+let selectedAdalaber = document.querySelector ('.selectedAdalaber');
+//selectedAdalaber.innerHTML = selectedAdalaber.innerHTML + firstAdalaber.innerHTML;
+selectedAdalaber.innerHTML = selectedAdalaber.innerHTML + secondAdalaber.innerHTML;
