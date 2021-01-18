87f91e4c6b21d3d890058a3fd45ee8929ae6105b
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-18 20:26:12 +0100

    Add modulo 2 lesson 1 trials folder

---
+++ modulo02-JS/1-intro-programacion/0-pruebas/js/main.js
@@ -0,0 +1,4 @@
+'use strict'; //sirve para mejorar la rapidez de ejecución del código y hará que el navegador nos muestre errores que, de no ponerla, no lo haría
+
+//Cada statement en una línea SIEMPRE
+document.querySelector('h1').innerHTML = '¡Hola mundo!';
