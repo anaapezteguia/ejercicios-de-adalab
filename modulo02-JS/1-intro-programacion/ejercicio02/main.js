b1cd96a0ca69350a38f5e346bf944f360dd30e5c
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-18 21:26:21 +0100

    Add exercise 02

---
+++ modulo02-JS/1-intro-programacion/ejercicio02/main.js
@@ -0,0 +1,4 @@
+'use strict';
+const myAddress = 'C/ Sario 31, 31180 Zizur Mayor (Navarra)'
+//const myAddress = 'C/ Palomitas 25'
+document.querySelector('p').innerHTML = 'Tu dirección es: ' + myAddress + '.';
