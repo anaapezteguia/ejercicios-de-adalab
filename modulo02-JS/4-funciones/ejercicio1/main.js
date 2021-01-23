4865b938dfa88822dcad9a8f862b6b799e8f24ed
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-23 18:21:53 +0100

    Add lesson 4 exercises 1 and 2

---
+++ modulo02-JS/4-funciones/ejercicio1/main.js
@@ -0,0 +1,5 @@
+'use strict';
+function multiply(first, second) {
+    return (first*second);
+}
+console.log (multiply(9, 8));
---
+++ modulo02-JS/4-funciones/ejercicio2/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function numberMedia (a,b,c,d){
+    return ((a+b+c+d)/4);
+}
+
+console.log(numberMedia(2, 8, 4, 6));
