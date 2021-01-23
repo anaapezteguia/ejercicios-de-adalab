761fbea4b6941ae9b37c6c27693681a4e60d4a9a
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-23 19:38:02 +0100

    Add exercise 03

---
+++ modulo02-JS/2-intro-programacion-2/ejercicio03/main.js
@@ -0,0 +1,9 @@
+'use strict';
+const kgKiwi= 5;
+const kgPeras= 2;
+const kgUvas= 4;
+const misKiwis= kgKiwi*2;
+const misPeras= kgPeras*3;
+const misUvas= kgUvas/2;
+const sumaTotal= misKiwis + misPeras + misUvas;
+console.log ('Me he gastado '+ sumaTotal + '€ en la frutería.');
