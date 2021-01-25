43ee76c73fed3e48ebdad7057dc7bbaa93a5c1e4
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-25 11:33:19 +0100

    Add comments in JS

--- modulo02-JS/5-eventos/ejercicio5/js/main.js
+++ modulo02-JS/5-eventos/ejercicio5/js/main.js
@@ -7,11 +7,11 @@ function changeBg (event) {
     let pressedKey = event.code;
     if (pressedKey === 'KeyR') {
         myPage.classList.add('red');
-        myPage.classList.remove('purple');
+        //myPage.classList.remove('purple');
         text.innerHTML = 'BIEEEEEEN!!! Has pulsado la tecla R'
     } else if (pressedKey === 'KeyM'){
         myPage.classList.add('purple');
-        myPage.classList.remove('red');
+        // myPage.classList.remove('red');
         text.innerHTML = 'BIEEEEEEN!!! Has pulsado la tecla M'
     } else {
         text.classList.add('fontColor');
