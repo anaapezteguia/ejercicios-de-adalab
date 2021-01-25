16de812aa93127b70a53800a66f4f02a98776a22
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-25 13:33:54 +0100

    Add new options to solve the same problem :)

--- modulo02-JS/5-eventos/ejercicio3/main.js
+++ modulo02-JS/5-eventos/ejercicio3/main.js
@@ -2,7 +2,20 @@
 const lorem = document.querySelector('.js-lorem');
 const lipsum = document.querySelector('.js-lipsum');
 
-function createLipsum() {
-    lipsum.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quis ab fugiat iste optio quod? Deleniti quia eligendi dolores adipisci perspiciatis commodi ullam optio ab totam, ipsa id repellendus quidem!';
+// function createLipsum() {
+//     lipsum.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quis ab fugiat iste optio quod? Deleniti quia eligendi dolores adipisci perspiciatis commodi ullam optio ab totam, ipsa id repellendus quidem!';
+//   }
+
+const main = document.querySelector('main');
+
+  // function createLipsum() {
+  //     const lipsum = '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quis ab fugiat iste optio quod? Deleniti quia eligendi dolores adipisci perspiciatis commodi ullam optio ab totam, ipsa id repellendus quidem!</p>';
+  //     main.innerHTML += lipsum;
+  //   }
+
+  //funcion arrow
+  const createLipsum = () => {
+    const lipsum = '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quis ab fugiat iste optio quod? Deleniti quia eligendi dolores adipisci perspiciatis commodi ullam optio ab totam, ipsa id repellendus quidem!</p>';
+    main.innerHTML += lipsum;
   }
   lorem.addEventListener('mouseover', createLipsum);
