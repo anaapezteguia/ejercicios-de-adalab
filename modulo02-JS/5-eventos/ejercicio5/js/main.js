3570cc969dfd9e85aa0c4d2a9367fda7cf942fd3
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-02-07 11:31:46 +0100

    Update exercise 5 with new functionalities

--- modulo02-JS/5-eventos/ejercicio5/js/main.js
+++ modulo02-JS/5-eventos/ejercicio5/js/main.js
@@ -1,21 +1,23 @@
 'use strict';
 const myPage = document.querySelector('.js-body');
 const text = document.querySelector('.js-message');
-
+text.classList.add('fontColor');
 
 function changeBg (event) {
     let pressedKey = event.code;
     if (pressedKey === 'KeyR') {
         myPage.classList.add('red');
-        //myPage.classList.remove('purple');
+        text.classList.remove('fontColor');
         text.innerHTML = 'BIEEEEEEN!!! Has pulsado la tecla R'
     } else if (pressedKey === 'KeyM'){
         myPage.classList.add('purple');
-        // myPage.classList.remove('red');
+        text.classList.remove('fontColor');
         text.innerHTML = 'BIEEEEEEN!!! Has pulsado la tecla M'
     } else {
         text.classList.add('fontColor');
         text.innerHTML = 'La tecla pulsada no hace nada :('
+        myPage.classList.remove('red');
+        myPage.classList.remove('purple');
     }
 }
 
